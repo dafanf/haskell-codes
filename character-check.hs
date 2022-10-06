a055f9719c08aea2519861f45b311b005ef30e82
dafanf <dafanurulfauziansyah97@gmail.com>
2022-10-06 22:46:29 +0700

    delete isAsciiDigit and isChar function

--- character-check.hs
+++ character-check.hs
@@ -1,10 +1,4 @@
 import Data.Char
-isChar :: Char -> Bool
-isChar c = c `elem` ['a'..'z'] || c `elem` ['A'..'Z']
-
-isAsciiDigit :: Char -> Bool
-isAsciiDigit c = c `elem` [' '..'~']
-
 uppercase :: String -> String
 uppercase = map toUpper
 
